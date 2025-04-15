801a15d18d7a93c960a58b94ef77aad8a5b5b868
Devarsh Thaker <devarsht@gmail.com>
2025-04-14 17:42:25 -0700

    basic user authentication api and db

--- edu-genius-api/server.js
+++ edu-genius-api/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
 
+//user routers and db connection:
+const { connectToDB, createUsersCollection } = require("./db/database.js");
+
 const app = express();
 
 // Morgan middleware for logging requests
@@ -12,7 +15,17 @@ app.use(express.json());
 
 app.use(morgan("dev"));
 
-// Start the server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+// Connect to MongoDB
+connectToDB().then(() => {
+  //Create users collection
+  createUsersCollection();
+
+  // Routes
+  const userRoutes = require("./routes/userRoutes");
+  app.use("/users", userRoutes);
+
+  // Start the server
+  app.listen(3000, () => {
+    console.log("Server started on port 3000");
+  });
 });
