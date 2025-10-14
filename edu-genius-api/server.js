0e5bc3e6460f097cc7cc6d49f197acac5c6523c7
MadhuriAPatil <madhuripatilgonal1@gmail.com>
2025-10-14 16:48:44 -0700

    removed /lib folder and used existing prisma

--- edu-genius-api/server.js
+++ edu-genius-api/server.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const prisma = require("./lib/prisma");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const { connectToDB, createUsersCollection } = require("./db/database");
 
