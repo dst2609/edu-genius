af7f66caa17dd9a4cb065038a13baf790da25231
Devarsh Thaker <devarsht@gmail.com>
2025-11-29 17:24:54 -0800

    fixed login typo

--- edu-genius-ui/src/Components/Login/Login.jsx
+++ edu-genius-ui/src/Components/Login/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Alert } from "@mui/material";
-import "./login.css";
+import "./Login.css";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL.replace(/\/+$/, "");
 
